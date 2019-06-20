b1675218f250019f9fee9816e755af38701fb32e
MatthewNobes <mattnobes.personal@protonmail.com>
2019-06-20 22:23:00 +0100

    The final version I uploaded for my course work.

---
+++ JavaCourseworkUP886226/src/javacourseworkup886226/Global.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacourseworkup886226;
+
+/**
+ *
+ * @author Matt
+ */
+public class Global {
+    /* 
+    Defines a global variable for the orde class so al lthe orders are on one class.
+    */
+    public static OrderClass af = new OrderClass();
+}
